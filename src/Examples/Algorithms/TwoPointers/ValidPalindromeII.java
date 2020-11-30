b7330c8ecdd67a5d35c8f97d3ecf3bd425c37123
Wei Dai <usadaiwei@gmail.com>
2020-11-30 10:24:15 -0600

    Add ValidPalindromeII

--- src/Examples/Algorithms/TwoPointers/ValidPalindromeII.java
+++ src/Examples/Algorithms/TwoPointers/ValidPalindromeII.java
@@ -9,10 +9,19 @@ Explanation: You could delete the character 'c'.
  */
 public class ValidPalindromeII {
     public boolean validPalindrome(String s){
-
+        for (int i = 0,  j = s.length() - 1; i < j; i++, j--){
+            if (s.charAt(i) != s.charAt(j)){
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+        }
+        return true;
     }
 
-    public boolean isPalindrome(int small, int large){
-
+    public boolean isPalindrome(String s, int small, int large){
+        while (small < large){
+            if (s.charAt(small++) != s.charAt(large--))
+                return false;
+        }
+        return true;
     }
 }
