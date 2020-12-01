b5039fd8b7134f61fd6727d3b0b74e2d98f205c2
Wei Dai <usadaiwei@gmail.com>
2020-12-01 16:01:44 -0600

    Add QuickSort

---
+++ src/Examples/Algorithms/Algorithm/QuickSort.java
@@ -0,0 +1,40 @@
+package Examples.Algorithms.Algorithm;
+
+/* Quick Sort: Find a mid, put smaller elements to its left side, put larger elements to its right side.
+ Left: point to the first element of array
+ Right: point to the last element of array
+ */
+public class QuickSort {
+    public static void quickSort(int[] array, int L, int R){
+        int i = L;
+        int j = R;
+        int mid = array[(L + R) / 2];
+
+        while (i <= j){
+
+            while (mid > array[i]){
+                i++;
+            }
+
+            while (mid < array[j]){
+                j--;
+            }
+
+            if (i <= j){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (L < j){
+            quickSort(array, L, j);
+        }
+
+        if (i < R){
+            quickSort(array, i, R);
+        }
+    }
+}
